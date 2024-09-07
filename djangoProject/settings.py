fd662c6d694b988690136b9baaab0c688d4511b4
jm-jung <jmsk7554@gmail.com>
2024-09-07 16:12:45 +0900

    install isort

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-2arpl)lnf_l_*z=#mgxre1!=q9r)tb4uvrgi5(!p*9lqm7t5w7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str]  = []
 
 
 # Application definition
